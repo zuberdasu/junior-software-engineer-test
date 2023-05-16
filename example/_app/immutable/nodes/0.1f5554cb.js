a79b270de247fa5e863c425cb8a808a302ee1ce1
sutan mufti <sutan@ambersideenergy.com>
2023-05-16 12:04:26 +0100

    fixing example. allborough to allboroughs

---
+++ example/_app/immutable/nodes/0.1f5554cb.js
@@ -0,0 +1,25 @@
+import{S as b,i as k,s as v,C as w,k as m,q as L,a as $,D as I,l as u,m as S,r as G,h,c as T,n as o,E as d,b as E,F as K,G as N,H as U,g as V,d as M}from"../chunks/index.190ecc22.js";import{b as q}from"../chunks/paths.310cb4a5.js";function x(f){let a,l,i,n,s,g,p,c;const _=f[1].default,t=w(_,f,f[0],null);return{c(){a=m("link"),l=m("link"),i=m("link"),n=m("link"),s=m("style"),g=L(`body {\r
+            font-family: Lato, 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;\r
+            height: 100vh;\r
+            width: 100vw;\r
+            /* border: 1px solid red; */\r
+            margin: 0;\r
+            background-color: rgb(235, 235, 235);\r
+\r
+        }\r
+\r
+        h1 {\r
+            font-family: Merriweather, 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;\r
+        }`),p=$(),t&&t.c(),this.h()},l(e){const r=I("svelte-t2lp2l",document.head);a=u(r,"LINK",{rel:!0,href:!0}),l=u(r,"LINK",{rel:!0,href:!0}),i=u(r,"LINK",{href:!0,rel:!0}),n=u(r,"LINK",{rel:!0,type:!0,href:!0}),s=u(r,"STYLE",{});var y=S(s);g=G(y,`body {\r
+            font-family: Lato, 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;\r
+            height: 100vh;\r
+            width: 100vw;\r
+            /* border: 1px solid red; */\r
+            margin: 0;\r
+            background-color: rgb(235, 235, 235);\r
+\r
+        }\r
+\r
+        h1 {\r
+            font-family: Merriweather, 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;\r
+        }`),y.forEach(h),r.forEach(h),p=T(e),t&&t.l(e),this.h()},h(){o(a,"rel","preconnect"),o(a,"href","https://fonts.googleapis.com"),o(l,"rel","preconnect"),o(l,"href","https://fonts.gstatic.com"),o(i,"href","https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Merriweather:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap"),o(i,"rel","stylesheet"),o(n,"rel","icon"),o(n,"type","image/x-icon"),o(n,"href",`${q}/ael-logo.png`)},m(e,r){d(document.head,a),d(document.head,l),d(document.head,i),d(document.head,n),d(document.head,s),d(s,g),E(e,p,r),t&&t.m(e,r),c=!0},p(e,[r]){t&&t.p&&(!c||r&1)&&K(t,_,e,e[0],c?U(_,e[0],r,null):N(e[0]),null)},i(e){c||(V(t,e),c=!0)},o(e){M(t,e),c=!1},d(e){h(a),h(l),h(i),h(n),h(s),e&&h(p),t&&t.d(e)}}}function C(f,a,l){let{$$slots:i={},$$scope:n}=a;return f.$$set=s=>{"$$scope"in s&&l(0,n=s.$$scope)},[n,i]}class H extends b{constructor(a){super(),k(this,a,C,x,v,{})}}export{H as component};
